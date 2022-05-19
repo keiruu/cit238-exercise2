3e734e235d64eb7d7f3b29ebc3f24585dd27ac57
Abigail Kaye Unating <abbyunat@gmail.com>
2022-05-19 17:54:19 +0800

    Removed old import

--- exercise2/lib/main.dart
+++ exercise2/lib/main.dart
@@ -3,7 +3,6 @@ import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'post.dart';
-import 'bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
